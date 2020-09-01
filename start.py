ebc19490f58eee977f22cf19a80441000f706545
fstetic <filip.stetic74@gmail.com>
2020-09-01 14:55:31 +0200

    Deleted Flask stuff and moved html and css files to separate folder

--- start.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint, render_template
-
-bp = Blueprint('game', __name__)
-@bp.route('/play', methods=('GET', 'POST'))
-def start_game():
-	return render_template("template.html")
