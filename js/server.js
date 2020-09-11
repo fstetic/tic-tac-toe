f5a581469d2b7a7ffa2c0c2af15db7b1679ec980
fstetic <filip.stetic74@gmail.com>
2020-09-11 18:10:33 +0200

    Added files for deployment and updated port for listening

--- js/server.js
+++ js/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path')
 const app = new express();
+let helmet = require('helmet')
 
+app.use(helmet())
 app.use(express.static(__dirname));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
@@ -10,4 +12,4 @@ app.set('views', path.join(__dirname));
 app.get('/', function(req, res){
 	res.contentType('text/html')
 	res.render('template')
-}).listen(8080);
+}).listen(process.env.PORT);
